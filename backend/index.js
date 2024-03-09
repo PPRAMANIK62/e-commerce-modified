6a83edec6da74d281f8ae2f73fb6601efeba9a28
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-03-09 11:39:29 +0530

    Initial Commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,25 @@
-console.log("Hello World!")
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+
+import connectDB from "./config/db.js";
+
+dotenv.config({ path: "backend/.env" });
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
